4bb7f6bd517b2e32402aaf9bce81fa8b99f7688d
umipzhaw <umikerphilipp1993@gmail.com>
2024-04-01 10:09:37 +0200

    Test2: Prüfe, ob drei Busfahrer, die sich an unterschiedlichen Haltestellen treffen, ihre Geschichten nach einigen Runden austauschen.

--- Unittest/DevOpsDemo-main/backend/src/test/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatschTest.java
+++ Unittest/DevOpsDemo-main/backend/src/test/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatschTest.java
@@ -12,4 +12,13 @@ public class BusfahrerKlatschTest {
         int[][] buslinien = {{1, 2, 3}, {1, 8, 5}};
         assertEquals("1", klatsch.berechneAustauschDauer(buslinien));
     }  
+
+    @Test
+    public void busfahrerTauschenGeschichtenNachMehrerenRundenAus() {
+        BusfahrerKlatsch klatsch = new BusfahrerKlatsch();
+        int[][] buslinien = {{1, 4, 7}, {2, 4, 6}, {3, 5, 7}};
+        // Annahme: Die Methode gibt die Runde zurück, in der der letzte Austausch stattfindet,
+        // unter Berücksichtigung der Wiederholung der Routen und indirekter Austausche.
+        assertEquals("5", klatsch.berechneAustauschDauer(buslinien));
+    }  
 }
