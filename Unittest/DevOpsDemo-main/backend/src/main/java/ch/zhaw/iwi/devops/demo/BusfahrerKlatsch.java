938fc9d72980138a31964d414e74bd8a23f66c99
umipzhaw <umikerphilipp1993@gmail.com>
2024-04-01 09:47:20 +0200

    Test1: Main Class "BusfahrerKlatsch.java daraus erstellt

--- Unittest/DevOpsDemo-main/backend/src/main/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatsch.java
+++ Unittest/DevOpsDemo-main/backend/src/main/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatsch.java
@@ -1,5 +1,17 @@
 package ch.zhaw.iwi.devops.demo;
 
 public class BusfahrerKlatsch {
-    
+
+    public String berechneAustauschDauer(int[][] buslinien) {
+        for (int i = 0; i < buslinien[0].length; i++) {
+            int haltestelle = buslinien[0][i];
+            for (int j = 0; j < buslinien[1].length; j++) {
+                if (haltestelle == buslinien[1][j]) {
+                    return String.valueOf(i + 1);
+                }
+            }
+        }
+        return "Keine gemeinsame Haltestelle gefunden";
+    }
 }
+    
