8f9702e7f154c95c6af6913db88db9ad64c4348b
umipzhaw <umikerphilipp1993@gmail.com>
2024-04-01 13:26:11 +0200

    Test 9: Prüfe, ob das Ergebnis korrekt ist, wenn alle Busfahrer von Beginn an alle Geschichten kennen.

--- Unittest/DevOpsDemo-main/backend/src/test/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatschTest.java
+++ Unittest/DevOpsDemo-main/backend/src/test/java/ch/zhaw/iwi/devops/demo/BusfahrerKlatschTest.java
@@ -85,5 +85,17 @@ public class BusfahrerKlatschTest {
         assertEquals("2", klatsch.berechneAustauschDauer(buslinien));
     } 
 
+    @Test
+    public void Geschickten_bekannt() {
+        BusfahrerKlatsch klatsch = new BusfahrerKlatsch();
+        int[][] buslinien = {{1, 4, 7}, {1, 4, 6}, {1, 4, 7}};
+        // Annahme: Die Methode gibt die Runde zurück, in der der letzte Austausch stattfindet,
+        // unter Berücksichtigung der Wiederholung der Routen und indirekter Austausche.
+        assertEquals("1", klatsch.berechneAustauschDauer(buslinien));
+    } 
+
+
+
+
 
 }
